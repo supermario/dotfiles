a115d2b2f39ced1176db1aa6d81092d8277ac945
Mario Rogic <hello@mario.net.au>
2017-02-04 13:41:58 +0000

    Forget how to upgrade postgres majors no more

---
+++ postgres/upgradepg.sh
@@ -0,0 +1,35 @@
+#!/usr/local/bin/bash
+set -x
+set -e
+
+# To be run after a brew update && brew upgrade posgres
+
+# Note: don't need to do migrations for minor semver upgrades i.e. 1.1.x++
+
+# @TODO make this script run the actual upgrade as well – next time we want to upgrade postgres
+
+brew services stop postgresql
+
+# Get current version numbers
+readarray -t versions <<< "$(ls -1 /usr/local/Cellar/postgresql/)"
+old=${versions[0]}
+new=${versions[1]}
+
+# @TODO should probably confirm at this point if we want to continue
+
+initdb /usr/local/var/postgres-$new -E utf8
+
+pg_upgrade \
+  -d /usr/local/var/postgres \
+  -D /usr/local/var/postgres-$new \
+  -b /usr/local/Cellar/postgresql/$old/bin/ \
+  -B /usr/local/Cellar/postgresql/$new/bin/ \
+  -v
+
+mv /usr/local/var/postgres /usr/local/var/postgres-$old-old
+mv /usr/local/var/postgres-$new /usr/local/var/postgres
+
+brew services start postgresql
+
+# @TODO make this script remove the old version of postgres with prompt?
+# rm -rf /usr/local/var/postgres-$old-old
